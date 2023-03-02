2b5274e7936d8e6fe7040b18a383ca1e6b7f788b
Kevin Jay <>
2023-03-02 12:06:00 -0500

    add .p10k.zsh to zsh directory

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,5 +3,6 @@
 set -e
 
 ln -sf "${DOTFILES_LOCATION}/zsh/.zshrc" "${HOME}/.zshrc"
+ln -sf "${DOTFILES_LOCATION}/zsh/.p10k.zsh" "${HOME}/.p10k.zsh"
 ln -sf "${DOTFILES_LOCATION}/zsh/benmatselby.zsh-theme" "${HOME}/.oh-my-zsh/custom/themes/"
 ln -sf "${DOTFILES_LOCATION}/zsh/powerlevel10k" "${HOME}/.oh-my-zsh/custom/themes/powerlevel10k"
